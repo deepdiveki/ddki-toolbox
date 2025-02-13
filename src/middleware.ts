b30bacddea123934e3ed7a3efa490425ad492375
Tim Philipp <info@deepdive-ki.de>
2025-02-13 11:33:14 +0100

    added middleware sitewide protect

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,25 +6,19 @@ export function middleware(request: NextRequest) {
   // 1. Get the session token from the cookies
   const sessionToken = request.cookies.get('next-auth.session-token')?.value;
 
-  // 2. Define protected routes
-  const protectedRoutes = ['/ki-chat']; // Add your protected routes here
-  const isProtectedRoute = protectedRoutes.some((route) =>
-    request.nextUrl.pathname.startsWith(route),
-  );
-
-  // 3. If the user is not authenticated and tries to access a protected route, redirect to login
-  if (isProtectedRoute && !sessionToken) {
+  // 2. If the user is not authenticated and tries to access a protected route, redirect to login
+  if (!sessionToken) {
     const loginUrl = "https://www.deepdive-ki.de/auth/signin";
     return NextResponse.redirect(loginUrl);
   }
 
-  // 4. If the user is authenticated and tries to access the login page, redirect to the profilpage
+  // 3. If the user is authenticated and tries to access the login page, redirect to the profilpage
   if (request.nextUrl.pathname.startsWith('/auth/signin') && sessionToken) {
     const profilUrl = "https://www.deepdive-ki.de/profil";
     return NextResponse.redirect(profilUrl);
   }
 
-  // 5. Allow the request to proceed if no conditions are met
+  // 4. Allow the request to proceed if no conditions are met
   return NextResponse.next();
 }
 
