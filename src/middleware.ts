5bf9c7dc2a04c0790f5e50fe673f1bb02a96c21c
deepdiveki <info@deepdive-ki.de>
2025-02-15 18:28:48 +0100

    reactivate site protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,10 +7,10 @@ export function middleware(request: NextRequest) {
   const sessionToken = request.cookies.get('next-auth.session-token')?.value;
 
   // 2. If the user is not authenticated and tries to access a protected route, redirect to login
-//   if (!sessionToken) {
-//     const loginUrl = "https://www.deepdive-ki.de/auth/signin";
-//     return NextResponse.redirect(loginUrl);
-//   }
+  if (!sessionToken) {
+    const loginUrl = "https://www.deepdive-ki.de/auth/signin";
+    return NextResponse.redirect(loginUrl);
+  }
 
   // 3. If the user is authenticated and tries to access the login page, redirect to the profilpage
   if (request.nextUrl.pathname.startsWith('/auth/signin') && sessionToken) {
